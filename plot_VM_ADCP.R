586da276d0faa8a103266a9b5df39508ffabb34b
Greenan <Blair.Greenan@dfo-mpo.gc.ca>
2023-06-07 13:40:28 -0300

    First commit

---
+++ plot_VM_ADCP.R
@@ -0,0 +1,20 @@
+# plot_VM_ADCP.R
+# Blair Greenan
+# Fisheries and Oceans Canada
+# 7 June 2023
+#
+# Description: this script generates a plot of the RV NB Palmer vessel-mounted ADCP
+#
+# load libraries
+library(oce)
+library(R.matlab)
+library(R.oo)
+library(tidyverse)
+library(lubridate)
+library(cmocean)
+library(interp)
+library(patchwork)
+
+# load to processed data file
+ADCP <- readMat("contour_uv.mat")
+
